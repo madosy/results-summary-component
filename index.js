a03b1464a861a2911f07c9a80d16b4338ce1c753
Dasom Heo <dasomee@outlook.com>
2023-03-10 15:42:49 -0500

    Fields are updated using imported json

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("I'm called from index.js")
+import data from './data.json' assert { type: "json" };
 
-const button = document.querySelector('.result-summary button')
+const criteriaFields = document.querySelectorAll('.criteria');
+criteriaFields.forEach((field, ind) => {
+    const icon = field.querySelector('img');
+    const fieldTitle = field.querySelector('h2');
+    const fieldScore = field.querySelector('.score');
+
+    icon.src = data[ind].icon;
+    icon.alt = `${data[ind].category} category icon`
+    fieldTitle.textContent = data[ind].category;
+    fieldScore.textContent = data[ind].score;
+})
